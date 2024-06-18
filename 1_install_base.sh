84d9c55382f50e482c9dee6840236c07f12cec6a
Mezentsev Denis <mezentsev.d@olytechspb.com>
2024-06-19 00:00:32 +0300

    added swap support

--- 1_install_base.sh
+++ 1_install_base.sh
@@ -2,9 +2,9 @@
 
 show_usage() {
     cat << EOF
-Usage: ${0} [-ain] path_to_disk root_size
+Usage: ${0} [-ain] path_to_disk swap_size
 Create 2 or 3 partions and install necessary packages
-When root_size equals 0, then no seprate home partition would be created
+When swap_size equals 0, then no seprate home partition would be created
 
 -h          Display help
 -a          Install AMD microcodes
@@ -19,7 +19,7 @@ options=$(getopt hainw ${*})
 if [ $? != 0 ] ; then show_usage; exit 1; fi
 eval set -- "${options}"
 
-PACKAGES=(base base-devel pacman-contrib linux linux-headers linux-firmware git neovim networkmanager openssh)
+PACKAGES=(base base-devel pacman-contrib linux-lts linux-lts-headers linux-firmware git neovim networkmanager openssh)
 NVME_PREFIX=""
 while true; do
     case $1 in
@@ -38,42 +38,47 @@ done
 [[ $# -lt 2 ]] && show_usage && exit 1;
 
 BLOCK_DEVICE="$1"
-ROOT_SIZE="$2"
+SWAP_SIZE="$2"
 
 [[ ! -b $BLOCK_DEVICE ]] && echo "Disk: $BLOCK_DEVICE should be a block device" && exit 1
-[[ ! "$ROOT_SIZE" =~ ^[0-9]+$ ]] && echo "Root size: $ROOT_SIZE should be an integer" && exit 1
+[[ ! "$SWAP_SIZE" =~ ^[0-9]+$ ]] && echo "Root size: $SWAP_SIZE should be an integer" && exit 1
 
 timedatectl set-ntp true
 
 sgdisk -Z $BLOCK_DEVICE
 
-sgdisk -n 0:0:+300M -t 0:ef00 $BLOCK_DEVICE
+sgdisk -n 0:0:+1G -t 0:ef00 $BLOCK_DEVICE
 
-if (( "$ROOT_SIZE" > 0 )); then
-  sgdisk -n 0:0:+"$ROOT_SIZE"G -t 0:8300 $BLOCK_DEVICE
-  sgdisk -n 0:0:0 -t 0:8300 $BLOCK_DEVICE
-else
-  sgdisk -n 0:0:0 -t 0:8300 $BLOCK_DEVICE
+if (( "$SWAP_SIZE" > 0 )); then
+  sgdisk -n 0:0:+"$SWAP_SIZE"G -t 0:8200 $BLOCK_DEVICE
 fi
+
+sgdisk -n 0:0:0 -t 0:8300 $BLOCK_DEVICE
 
 sgdisk -p $BLOCK_DEVICE
 
 partprobe $BLOCK_DEVICE
 
-mkfs.fat -F32 "${BLOCK_DEVICE}${NVME_PREFIX}"1
-mkfs.ext4 "${BLOCK_DEVICE}${NVME_PREFIX}"2
+BOOT_PARTITION="${BLOCK_DEVICE}${NVME_PREFIX}"1
+SWAP_PARTITION="${BLOCK_DEVICE}${NVME_PREFIX}"2
+ROOT_PARTITION="${BLOCK_DEVICE}${NVME_PREFIX}"2
 
-mount "${BLOCK_DEVICE}${NVME_PREFIX}"2 /mnt
+mkfs.fat -F32 "$BOOT_PARTITION"
+
+if (( "$SWAP_SIZE" > 0 )); then
+  mkswap "$SWAP_PARTITION"
+  ROOT_PARTITION="${BLOCK_DEVICE}${NVME_PREFIX}"3
+  swapon "$SWAP_PARTITION"
+fi
+
+mkfs.ext4 "$ROOT_PARTITION"
+
+mount "$ROOT_PARTITION" /mnt
 
 mkdir /mnt/boot
 mkdir /mnt/home
 
 mount "${BLOCK_DEVICE}${NVME_PREFIX}"1 /mnt/boot
-
-if (( "$ROOT_SIZE" > 0 )); then
-  mkfs.ext4 "${BLOCK_DEVICE}${NVME_PREFIX}"3
-  mount "${BLOCK_DEVICE}${NVME_PREFIX}"3 /mnt/home
-fi
 
 pacstrap /mnt "${PACKAGES[@]}"
 

d335d1891925fef3f758aed47ae7b0b41832fa5b
Mezentsev Denis <mezentsev.d@olytechspb.com>
2025-06-16 23:20:12 +0300

    next

--- handwritten/5_config_network.sh
+++ handwritten/5_config_network.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 systemctl disable systemd-networkd.socket
-systemctl enable systemd-resolved.service
+systemctl disable systemd-resolved.service
 systemctl enable NetworkManager.service
 systemctl enable systemd-timesyncd.service

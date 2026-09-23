ecf6bef25da568f70eecb8ec4c2bb8dc0894497e
Mezentsev Denis <deni.mez@marathonbetops.net>
2026-09-23 12:22:27 +0300

    next

--- 3_configure_base.sh
+++ 3_configure_base.sh
@@ -151,8 +151,6 @@ fi
 # systemd-boot
 # ---------------------------------------------------------------------------
 echo "#>> Installing systemd-boot"
-# /efi is formatted as FAT32, Unix file permissions doesn't work here, HOWEVER this can fix warning for just better health.
-chmod 700 /efi/loader
 bootctl install
 
 echo "#>> Installing /efi/loader/loader.conf"
